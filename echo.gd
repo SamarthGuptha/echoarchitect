6a5706766e17dfc5ea1368a2314226056b8609a1
Samarth Guptha <developervirusly@gmail.com>
2025-09-06 08:52:54 +0530

    feat: record and playback, basic controls

---
+++ echo.gd
@@ -0,0 +1,34 @@
+class_name Echo
+extends CharacterBody2D
+@onready var animated_sprite = $AnimatedSprite2D
+
+var playback_data = []
+var current_frame = 0
+var is_playing = false
+
+func start_playback(data: Array):
+	if data.is_empty():
+		queue_free()
+		return
+	playback_data = data
+	is_playing = true
+	global_position = playback_data[0].position
+	animated_sprite.animation = playback_data[0].animation
+	animated_sprite.flip_h = playback_data[0].flip_h
+
+
+func _physics_process(delta):
+	if not is_playing:
+		return
+	if current_frame >= playback_data.size():
+		print("Echo playback finished.")
+		is_playing = false
+		queue_free()
+		return
+	var frame_data = playback_data[current_frame]
+	
+	global_position = frame_data.position
+	animated_sprite.play(frame_data.animation)
+	animated_sprite.flip_h = frame_data.flip_h
+	
+	current_frame += 1
--- player.gd
+++ player.gd
@@ -4,22 +4,58 @@ signal spawn_echo(playback_data)
 const SPEED = 250.0
 const JUMP_VELOCITY = -350.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animated_sprite = $AnimatedSprite2D
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+var recording_data = []
+var is_recording = false
+
+func _physics_process(delta):
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += gravity*delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	var direction := Input.get_axis("move_left", "move_right")
+	velocity.x = direction*SPEED
+	update_animation(direction)
+	handle_recording()
+	if Input.is_action_just_pressed("playback"):
+		if not recording_data.is_empty():
+			emit_signal("spawn_echo", recording_data)
+			recording_data.clear()
 	move_and_slide()
+func handle_recording():
+	if Input.is_action_just_pressed("record"):
+		is_recording = true
+		recording_data.clear()
+		print("Rec started...")
+	if Input.is_action_just_released("record"):
+		is_recording = false
+		print("Recording stopped. frames recorded: ", recording_data.size())
+		
+	if is_recording:
+		record_current_frame()
+func record_current_frame():
+	var frame_data = {
+		"position": global_position,
+		"animation": animated_sprite.animation,
+		"flip_h": animated_sprite.flip_h
+	}
+	recording_data.append(frame_data)
+func update_animation(direction):
+	if direction>0:
+		animated_sprite.flip_h = false
+	elif direction<0:
+		animated_sprite.flip_h = true
+	
+	if not is_on_floor():
+		animated_sprite.play("jump")
+	else:
+		if direction !=0 :
+			animated_sprite.play("run")
+		else:
+			animated_sprite.play("idle")
+			
